45ddef9e9e49d7fc87764b5271609e16d6dae41f
Rachel_BARETTO <ap.rachel.bds.crp@gmail.com>
2019-05-23 12:46:40 +0200

    ok

--- TP_Web_reparTout/js/nav.js
+++ TP_Web_reparTout/js/nav.js
@@ -1,6 +1,8 @@
       let str='';
-      str+='<li><a href="/rbaretto/TP_Web_reparTout/">Catégories</a></li>';
-      str+='<li><a href="/rbaretto/TP_Web_reparTout/catalogue/">Catalogue</a></li>';
-      str+='<li><a href="/rbaretto/TP_Web_reparTout/clients/">Clients</a></li>';
+
+      str+='<li><a href="../">Catégories</a></li>';
+      str+='<li><a href="../catalogue/">Catalogue</a></li>';
+      str+='<li><a href="../clients/">Clients</a></li>';
       str+='<li><a href="#">Commandes</a></li>';
+
       document.getElementById('nav').innerHTML=str;
