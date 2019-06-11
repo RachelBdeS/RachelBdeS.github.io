7808e24a35a52a0a99121065cb41b8140a97b402
Rachel_BARETTO <ap.rachel.bds.crp@gmail.com>
2019-06-11 16:14:13 +0200

    ok

--- TP_Web_reparTout/sql/bdd.sql
+++ TP_Web_reparTout/sql/bdd.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS repartout;
+CREATE DATABASE repartout CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 USE repartout;
 
 -- Désactive les contraintes des clés étrangères
@@ -7,9 +10,63 @@ SET FOREIGN_KEY_CHECKS=0; /* Désactive le control des relations entre les table
 -- CREATION --
 -- -------- --
 
--- Vide, puis crée la table fournisseurs: 
+CREATE TABLE IF NOT EXISTS `fournisseurs` 
+(
+  `id_fournisseur` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_fournisseur` varchar(50) NOT NULL,
+  `tel_fournisseur` char(15) NOT NULL,
+  `adresse_fournisseur` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_fournisseur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `pneus` 
+(
+  `nom_pneu` varchar(40) NOT NULL,
+  `marque_pneu` varchar(50) NOT NULL,
+  `diametre_pneu` tinyint(3) UNSIGNED NOT NULL,
+  `structure_pneu` varchar(20) NOT NULL,
+  `largeur_pneu` int(11) NOT NULL,
+  `usage_pneu` varchar(20) NOT NULL,
+  `indice_charge_pneu` int(11) NOT NULL,
+  `indice_vitesse_pneu` int(11) NOT NULL,
+  `prix_achat_ht` decimal(7,2) NOT NULL,
+  `prix_vente_ht` decimal(7,2) NOT NULL,
+  `id_fournisseur` int(11) NOT NULL,
+  PRIMARY KEY (`nom_pneu`),
+  KEY `pneus_ibfk_1` (`id_fournisseur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `commandes`
+(
+	id_commande INT(11) AUTO_INCREMENT PRIMARY KEY,
+    date_commande DATETIME NOT NULL,
+    etat_commande VARCHAR(50) NOT NULL,
+    prix_total DECIMAL(15,2) NOT NULL,
+    paiement_valide INT(11) NOT NULL
+    
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE lignes_commande
+(
+	id_ligne_commande INT(11) AUTO_INCREMENT PRIMARY KEY,
+    quantite INT(11) UNSIGNED NOT NULL DEFAULT '0',
+    prix_unitaire DECIMAL(7,2) NOT NULL,
+	nom_pneu varchar(40) NOT NULL,
+	id_commande INT(11) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE lignes_commande
+ADD FOREIGN KEY(nom_pneu) REFERENCES pneus(nom_pneu),
+ADD FOREIGN KEY(id_commande) REFERENCES commandes(id_commande);
+
+-- ------------- --
+-- RENSEIGNEMENT --
+-- ------------- --
+
 -- DELETE FROM fournisseurs; /* Pour PHP my ADMIN : empeche l'option "verification des clées étrangères" de la section INSERESION ds=des BDD de PHPmyAdmin d'outrepasser la commande SET GLOBAL FOREIGN_KEY_CHECKS*/
 TRUNCATE TABLE fournisseurs; 
+-- DELETE FROM fournisseurs; /* Pour PHP my ADMIN */
 INSERT INTO fournisseurs
 (
 	tel_fournisseur, 
@@ -20,13 +77,8 @@ VALUES
 ('03.89.89.89.87', 'Pneus pour tous ', '11 rue de Mulhouse 68400 Riedisheim'),
 ('03.88.77.78.70', 'Pneumorama ', '141 avenue de Colmar 68100 Mulhouse');
 
-VALUES 
-('Pneus pour tous', '03.89.89.89.87', '11 rue de Mulhouse 68400 Riedisheim'),
-('Pneumorama ', '03.88.77.78.70', '141 avenue de Colmar 68100 Mulhouse');
-
--- Vide, puis crée la table pneus: 
+TRUNCATE TABLE pneus;
 -- DELETE FROM pneus; /* Pour PHP my ADMIN */
-TRUNCATE TABLE pneus; 
 INSERT INTO pneus 
 (
 	nom_pneu, 
@@ -47,6 +99,8 @@ VALUES
 ('GY21', 'Good Year', '17', 'radial', '195', '4 saisons', '3500', '240', '39', '109','1'),
 ('GY29', 'Good Year', '19', 'radial', '205', '4 saisons', '3500', '240', '49', '129','1');
 
+TRUNCATE TABLE commandes;
+-- DELETE FROM commandes; /* Pour PHP my ADMIN */
 INSERT INTO commandes
 (
 	id_commande, 
@@ -60,15 +114,17 @@ VALUES
 (2, '2019-06-02 15:30:27', 'livraison en cours', 0, 1),
 (3, '2019-06-05 07:42:10', 'en cours', 0, 0);
 
-INSERT INTO lignes_commande 
+TRUNCATE TABLE lignes_commande;
+-- DELETE FROM lignes_commande; /* Pour PHP my ADMIN */
+INSERT INTO lignes_commande
 (
-  id_ligne_commande, 
-  quantite, 
-  prix_unitaire, 
-  nom_pneu, 
-  id_commande
+	id_ligne_commande,
+	quantite,
+	prix_unitaire,
+	nom_pneu,
+	id_commande
 )
-VALUES
+VALUES 
 (1, 1, 49, 'MC25', 1),
 (2, 1, 95, 'MC28', 1),
 (3, 2, 109, 'GY21', 2),
@@ -78,6 +134,9 @@ VALUES
 (7, 1, 95, 'MC28', 3),
 (8, 1, 109, 'GY21', 3),
 (9, 2, 129, 'GY29', 3);
+
+-- Vide, puis crée la table pneus: 
+-- DELETE FROM pneus; /* Pour PHP my ADMIN */
 
 -- -------------------- --
 -- CALCUL DU PRIX TOTAL --
@@ -101,6 +160,11 @@ c.prix_total=coalesce(
 	),
   '0'
 );
+
+-- UPDATE commandes SET prixtotal=(SELECT SUM(prix_unitaire) FROM lignes_commande 
+-- WHERE commandes.id_commande=lignes_commandes.id_commandes) 
+
+-- --------------------------
 
 -- Rétablit les contraintes des clés étrangères
 SET FOREIGN_KEY_CHECKS=1;
--- TP_Web_reparTout/sql/liaison.php
+++ TP_Web_reparTout/sql/liaison.php
@@ -1,24 +1,33 @@
+<h1>Bonjour</h1>
+
 <?php
-$servername = "localhost";
-$username = "repartout";
-$password = "Cdi1234";
-$dbname = "repartout";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$saisie = "toi";
+echo '<h3>Salut, '.$saisie.' !</h3>'; // Est interprété plus rapidement.
+echo "<h3>Salut, $saisie !</h3>"; // Est plus sécurité.
 
-$sql = "ooo";
+// Si: http://localhost/<chemin>.php?nom=Dupont&age=53
+$nom = $_GET['nom'];
+$age = intval($_GET['age']);
+
+echo "Bienvenue, $nom. Vous avez $age ans. ";
 
 
-if (mysqli_query($conn, $sql)) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
+// $servername = "localhost";
+// $username = "repartout";
+// $password = "Cdi1234";
+// $dbname = "repartout";
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+// $sql = "ooo";
+// if (mysqli_query($conn, $sql)) {
+//     echo "Record updated successfully";
+// } else {
+//     echo "Error updating record: " . mysqli_error($conn);
+// }
+// mysqli_close($conn);
 ?>
