20ffdf11436579a82b9d28d3f3b783a8a92af852
Rachel_BARETTO <ap.rachel.bds.crp@gmail.com>
2019-06-12 16:59:37 +0200

    ok

---
+++ Exercices_PHP/CLI/welcome.cmd
@@ -0,0 +1,2 @@
+@echo off
+php welcome.php
--- Exercices_PHP/CLI/welcome.php
+++ Exercices_PHP/CLI/welcome.php
@@ -1,16 +1,73 @@
-<!-- C:\wamp64\www\rbaretto\Exercices_PHP -->
-
 <?php
-if (count($argv) > 1)
+
+// NB: CTRL+C = pause d'urgence en CLI
+
+// Chemin du programme à entrer dans le terminal : 
+// "C:\wamp64\www\rbaretto\Exercices_PHP\CLI". 
+
+// Si le tableau 'arguments count or argc" contient plus d'1 élément, 
+if ($argc>1)
 {
+  // Stock le 2e élément dans la variable arg
   $arg = $argv[1];
 }
+// Sinon
 else 
 {
-  echo 'Type your name: ';
-  $arg = fgets(STDIN);
+  // Menu (liste des options) en CLI
+  echo "CHOOSE A PROGRAM";
+  echo PHP_EOL.PHP_EOL;
+  echo '[1] Display your chosen name';
+  echo PHP_EOL.PHP_EOL;
+  echo "[2] Calaculate the sum of two chosen numbers";
+  echo PHP_EOL.PHP_EOL;
+  echo "[3] Calculate age from a chosen date of birth";
+  echo PHP_EOL.PHP_EOL;
+  echo "[4] Display files of a chosen repertory";
+  echo PHP_EOL.PHP_EOL;
+  do 
+  {
+    echo "YOU CHOICE: ";
+    // Récupère la saisie de l'utilisateur
+    $saisie = fgets(STDIN);
+    // pour debug
+    // exit($saisie);
+    // Selon la saisie utilisateur,
+    switch ($saisie) 
+    {
+      case "1": echo "PROGRAM 1";
+        echo "Type your name: ";
+        $name = fgets(STDIN);
+        echo "Welcome, $name!";
+        break;
+      case "2": echo "PROGRAM 2";
+        echo "Type the first number: ";
+        $nb1 = fgets(STDIN);
+        echo "Type the second number: ";
+        $nb2 = fgets(STDIN);
+        $sum = $nb1+$nb2;
+        echo "$nb1+$nb2=$sum";
+        break;
+      case "3": echo "PROGRAM 3";
+        echo "Type a day of birth (use two digits): ";
+        $DD = fgets(STDIN);
+        echo "Type a month of birth (use two digits): ";
+        $MM = fgets(STDIN);
+        echo "Type a year of birth (use four digits): ";
+        $YYYY = fgets(STDIN);
+        $age = "$DD/$MM/$YYYY";
+        echo "This person is $age.";
+        echo "Well, they would be if you played by the rules...";
+        break;
+      case "4": echo "PROGRAM 4";
+      echo "Work in progress. ooo";
+        break;
+      case "exit": echo "Good bye !";
+        break;
+      default: echo "(This choice is not avaliable.)";    
+      echo "\n\n";
+    }
+  }
+  while (($saisie!="1") && ($saisie!="2") && ($saisie!="3") && ($saisie!="4") && ($saisie!="exit"));
 }
-
-echo PHP_EOL . "Welcome ".trim($arg). PHP_EOL;
-
 ?>
