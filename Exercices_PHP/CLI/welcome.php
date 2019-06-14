9c0a012544717a08eb84ab0109a8735cfd2684cf
Rachel_BARETTO <ap.rachel.bds.crp@gmail.com>
2019-06-14 10:56:12 +0200

    ok

--- Exercices_PHP/CLI/welcome.php
+++ Exercices_PHP/CLI/welcome.php
@@ -1,17 +1,13 @@
 <?php
-// NB: CTRL+C = pause d'urgence en CLI
-// NB: exit($saisie); /* Pour DEBUG : Affiche du contenu et stop le programme de suite après */
+/* NB: echo var_export($argv, true); affiche la liste du tableau d'arguments de la dernière ligne de commande (pour DEBUG) */
+// NB: CTRL+C = pause d'urgence en CLI 
+/* NB: exit($saisie); affiche du contenu et stop le programme de suite après (pour DEBUG) */
 // ------------------------------------------------
 // Chemin du programme à entrer dans le terminal : 
 // "C:\wamp64\www\rbaretto\Exercices_PHP\CLI". 
 
-echo var_export($argv, true);
-// Si le tableau 'arguments count or argc" contient plus d'1 élément, 
-if ($argc>1)
-{
-  // Stock le 2e élément dans la variable arg
-  $arg=$argv[1];
-}
+/* PHP en CLI • SCRIPT MODELE POUR MA FAQ */
+
 // Menu (liste des options) en CLI
 echo "CHOOSE A PROGRAM";
 echo PHP_EOL.PHP_EOL;
@@ -29,17 +25,29 @@ do
   echo "YOU CHOICE: ";
   $saisie = trim(fgets(STDIN)); /* Il est nécessaire de trimer le retour chariot pour qu'il ne soit pas pris en compte */
   
+  echo chr(27).chr(91).'H'.chr(27).chr(91).'J';
+
   // Selon la saisie utilisateur,
-  switch ($saisie) 
+  switch ($saisie)
   {
     case "1": echo "PROGRAM 1";
-      echo "Type your surname: ";
-      $surname = trim(fgets(STDIN));
-      echo "Type your family name: ";
-      $name = trim(fgets(STDIN));
+      echo "\n\n";
+      // Si un paramètre est saisi a la fin de la ligne de commande d'execution de ce script, 
+      if ($argc>1) 
+      {
+        $surname = $argv[1];
+        $name = $argv[2];
+      }
+      else {
+        echo "Type your surname: ";
+        $surname = trim(fgets(STDIN));
+        echo "Type your family name: ";
+        $name = trim(fgets(STDIN));
+      }
       echo "Welcome, $surname $name!";
       break;
     case "2": echo "PROGRAM 2";
+      echo "\n\n";
       echo "Type the first number: ";
       $nb1 = trim(fgets(STDIN));
       echo "Type the second number: ";
@@ -48,6 +56,7 @@ do
       echo "$nb1+$nb2=$sum";
       break;
     case "3": echo "PROGRAM 3";
+      echo "\n\n";
       echo "Type a day of birth (use two digits): ";
       $DD = trim(fgets(STDIN));
       echo "Type a month of birth (use two digits): ";
@@ -59,7 +68,8 @@ do
       echo "Well, they would be if you played by the rules...";
       break;
     case "4": echo "PROGRAM 4";
-    echo "Work in progress. ooo";
+      echo "\n\n";
+      echo "Work in progress. ooo";
       break;
     case "exit": echo "Good bye !";
       break;
@@ -67,6 +77,5 @@ do
     echo "\n\n";
   }
 }
-while (($saisie!="1") && ($saisie!="2") && ($saisie!="3") && ($saisie!="4") && ($saisie!="exit"));
-
+while ($saisie!="exit");
 ?>
